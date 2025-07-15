54c639708c069116c22bd79c72163e45b676cac7
Sanjit Dutta <sanji.dutta.bdn@gmail.com>
2025-07-15 08:17:45 +0530

    my first springboot app commit

---
+++ src/main/java/com/myapp/helloapp/HelloController.java
@@ -0,0 +1,18 @@
+package com.myapp.helloapp;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "My First SpringBoot App";
+    }
+    
+    }
+    
+    
+
---
+++ src/test/java/com/myapp/helloapp/HelloappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapp.helloapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
