7e67f2cd652debb2536724e64315903b51fdaf5a
Sanjit Dutta <sanji.dutta.bdn@gmail.com>
2025-07-15 09:23:46 +0530

    My Sceond change

--- src/main/java/com/myapp/helloapp/HelloController.java
+++ src/main/java/com/myapp/helloapp/HelloController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HelloController {
     @GetMapping("/hello")
     public String sayHello(){
-        return "My First SpringBoot App";
+        return "My First SpringBoot Ap that will be used to deploy at azure appservice using Git Hub Action";
     }
     
     }
