1379c82e94a9b74f0e89a937e079f650a88bf81e
Sanjit Dutta <sanji.dutta.bdn@gmail.com>
2025-07-17 00:48:28 +0530

    Deployment Center reconfigured My Slot Deletion

--- src/main/java/com/myapp/helloapp/HelloController.java
+++ src/main/java/com/myapp/helloapp/HelloController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HelloController {
     @GetMapping("/hello")
     public String sayHello(){
-        return "My First SpringBoot Ap that will be used to deploy at azure appservice using Git Hub Action Slot Wise";
+        return "My First SpringBoot Ap that will be used to deploy at azure appservice using Git Hub Action Slot Wise Devs";
     }
     
     }
